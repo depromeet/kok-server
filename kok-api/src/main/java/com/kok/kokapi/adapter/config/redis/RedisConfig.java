155e16dd8856627228dbe8f14d2affc67c05cf31
minseokey <lms990427@kookmin.ac.kr>
2025-02-18 18:43:48 +0900

    🔧config: use default spring redis setting

--- kok-api/src/main/java/com/kok/kokapi/adapter/config/redis/RedisConfig.java
+++ kok-api/src/main/java/com/kok/kokapi/adapter/config/redis/RedisConfig.java
@@ -1,13 +1,9 @@
 package com.kok.kokapi.adapter.config.redis;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -17,36 +13,10 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.data.redis.port}")
-    private int redisPort;
-
     @Bean
-    public RedisConnectionFactory redisStandaloneConnectionFactory() {
-        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(redisHost, redisPort);
-        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-                // TODO: 추가사항 추가. (명령어 타임아웃, 종료 타임아웃)
-                .build();
-        return new LettuceConnectionFactory(redisConfig, clientConfig);
-    }
-
-//    // 페일오버 가능한 Redis Sentinel 설정
-//    @Bean
-//    public RedisConnectionFactory redisSentinelConnectionFactory() {
-//        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
-//                .master("") // Sentinel에서 설정한 master 이름
-//                .sentinel("host1", 1234) //Sentinel1 서버 정보
-//                .sentinel("host2", 1234); // Sentinel2 서버 정보
-//        return new LettuceConnectionFactory(sentinelConfig);
-//    }
-
-
-    @Bean(name = "redisTemplate")
-    public RedisTemplate<UUID, Object> redisTemplate() {
+    public RedisTemplate<UUID, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<UUID, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisStandaloneConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
 
         // Key Serializer 설정 (UUID -> String 변환)
         redisTemplate.setKeySerializer(new StringRedisSerializer());
