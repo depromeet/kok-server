1400fb8be91eb38fd2f19a1e9a5a2d69244e164f
yunyoung1819 <yunyoung1819@hanmail.net>
2025-02-18 13:23:36 +0900

    :sparkles: feat: implement room create feature - Added Room domain model and factory method - Added CreateRoomUseCase with persistence adapter. - Added RoomController to handle room creation request

---
+++ kok-api/src/main/java/com/kok/kokapi/room/adapter/inbound/rest/RoomController.java
@@ -0,0 +1,30 @@
+package com.kok.kokapi.room.adapter.inbound.rest;
+
+import com.kok.kokapi.room.adapter.inbound.dto.CreateRoomRequest;
+import com.kok.kokcore.domain.Room;
+import com.kok.kokcore.usecase.CreateRoomUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rooms")
+@RequiredArgsConstructor
+public class RoomController {
+
+    private final CreateRoomUseCase createRoomUseCase;
+
+    @PostMapping("/create")
+    public ResponseEntity<Room> createRoom(@RequestBody CreateRoomRequest request) {
+        Room room = createRoomUseCase.createRoom(
+                request.getRoomName(),
+                request.getCapacity(),
+                request.getHostProfile(),
+                request.getPassword()
+        );
+        return ResponseEntity.ok(room);
+    }
+}
