57e8ce2dae59c5d9febd81656d676ad24321e282
yunyoung1819 <yunyoung1819@hanmail.net>
2025-03-05 01:46:07 +0900

    :sparkles: feat: setting scraper module

---
+++ kok-scraper/src/main/java/com/kok/KokScraperApplication.java
@@ -0,0 +1,13 @@
+package com.kok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KokScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KokScraperApplication.class, args);
+    }
+
+}
