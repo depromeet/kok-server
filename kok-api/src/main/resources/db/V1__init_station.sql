bf40e655f71ed52c1fe7b45d694da41f09f14268
linirini <2001yerin@naver.com>
2025-03-07 11:20:22 +0900

    :recycle: refactor: normalize flyway ddl station table

--- kok-api/src/main/resources/db/V1__init_station.sql
+++ kok-api/src/main/resources/db/V1__init_station.sql
@@ -1,11 +1,17 @@
-CREATE TABLE station
-(
-    id         BIGINT          NOT NULL AUTO_INCREMENT,
-    station_id BIGINT          NOT NULL,
-    name       VARCHAR(255)    NOT NULL,
-    route      VARCHAR(255)    NOT NULL,
-    latitude   DECIMAL(16, 14) NOT NULL,
-    longitude  DECIMAL(17, 14) NOT NULL,
-    priority BIGINT NOT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE station (
+                         id BIGINT NOT NULL AUTO_INCREMENT,
+                         name VARCHAR(20) NOT NULL,
+                         latitude DECIMAL(16, 14) NOT NULL,
+                         longitude DECIMAL(17, 14) NOT NULL,
+                         priority BIGINT NOT NULL,
+                         PRIMARY KEY (id)
+);
+
+CREATE TABLE route (
+                       id BIGINT NOT NULL AUTO_INCREMENT,
+                       code BIGINT NOT NULL,
+                       station_id BIGINT NOT NULL,
+                       name VARCHAR(20) NOT NULL,
+                       PRIMARY KEY (id),
+                       FOREIGN KEY (station_id) REFERENCES station(id)
 );
