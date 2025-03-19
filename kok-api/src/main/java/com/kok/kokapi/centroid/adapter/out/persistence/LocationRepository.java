d4571c094399b9dada747d0497877ff62cb5a062
linirini <2001yerin@naver.com>
2025-03-20 07:57:48 +0900

    :recycle: refactor: remove unused method

--- kok-api/src/main/java/com/kok/kokapi/centroid/adapter/out/persistence/LocationRepository.java
+++ kok-api/src/main/java/com/kok/kokapi/centroid/adapter/out/persistence/LocationRepository.java
@@ -63,6 +63,4 @@ public interface LocationRepository extends JpaRepository<Location, Long> {
         ORDER BY angle
         """, nativeQuery = true)
     List<Location> findConvexHull(@Param("roomId") String roomId);
-
-    long countByRoomId(String roomId);
 }
