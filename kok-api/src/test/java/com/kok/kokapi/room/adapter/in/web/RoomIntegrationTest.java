670c1f1fc663b001293f70eb208fb8d70c51bab6
linirini <2001yerin@naver.com>
2025-03-20 08:00:30 +0900

    :recycle: refactor: add eol

--- kok-api/src/test/java/com/kok/kokapi/room/adapter/in/web/RoomIntegrationTest.java
+++ kok-api/src/test/java/com/kok/kokapi/room/adapter/in/web/RoomIntegrationTest.java
@@ -34,14 +34,15 @@ class RoomIntegrationTest extends IntegrationTest {
                     .assertThat().statusCode(201)
                     .extract().body().jsonPath().getObject("data", RoomCreateResponse.class))),
             DynamicTest.dynamicTest("방장이 출발지 정보를 입력한다.",
-                () -> inputLocation(createRoomResponse.get().id(), createRoomResponse.get().member().id())),
+                () -> inputLocation(createRoomResponse.get().id(),
+                    createRoomResponse.get().member().id())),
             DynamicTest.dynamicTest("약속방 정보를 조회해보면 아직 참여자가 다 들어오지 않았기 때문에 voteMode는 false이다.",
                 () -> getRoomDetail(createRoomResponse.get().id(), false)),
             DynamicTest.dynamicTest("팔로워가 약속방에 참여한다.",
                 () -> joinRoomResponse.set(RestAssured.given().log().all()
                     .contentType(ContentType.JSON)
                     .body(new JoinRoomParticipantRequest("profile", "follower"))
-                    .when().post("/v1/api/rooms/"+createRoomResponse.get().id()+"/join")
+                    .when().post("/v1/api/rooms/" + createRoomResponse.get().id() + "/join")
                     .then().log().all()
                     .assertThat().statusCode(200)
                     .extract().body().jsonPath().getObject("data", JoinRoomResponse.class))),
@@ -57,7 +58,8 @@ class RoomIntegrationTest extends IntegrationTest {
     private static void inputLocation(String roomId, String memberId) {
         RestAssured.given().log().all()
             .contentType(ContentType.JSON)
-            .body(new LocationRequest(roomId, memberId, new BigDecimal("37.49794"), new BigDecimal("127.02758")))
+            .body(new LocationRequest(roomId, memberId, new BigDecimal("37.49794"),
+                new BigDecimal("127.02758")))
             .when().post("/v1/api/locations")
             .then().log().all()
             .assertThat().statusCode(200);
