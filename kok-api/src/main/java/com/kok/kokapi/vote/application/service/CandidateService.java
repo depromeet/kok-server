dee7f45325972cd060f01396718924f6856a8818
linirini <2001yerin@naver.com>
2025-03-25 02:12:07 +0900

    :sparkle: feat: implement get candidate usecase

---
+++ kok-api/src/main/java/com/kok/kokapi/vote/application/service/CandidateService.java
@@ -0,0 +1,32 @@
+package com.kok.kokapi.vote.application.service;
+
+import com.kok.kokcore.station.application.usecase.GetRecommendStationUseCase;
+import com.kok.kokcore.station.domain.entity.Station;
+import com.kok.kokcore.vote.application.port.out.LoadCandidatePort;
+import com.kok.kokcore.vote.application.port.out.SaveCandidatePort;
+import com.kok.kokcore.vote.domain.Candidate;
+import com.kok.kokcore.vote.usecase.GetCandidateUseCase;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CandidateService implements GetCandidateUseCase {
+
+    private final SaveCandidatePort saveCandidatePort;
+    private final LoadCandidatePort loadCandidatePort;
+    private final GetRecommendStationUseCase getRecommendStationUseCase;
+
+    @Override
+    public List<Candidate> getCandidate(String roomId) {
+        if (!loadCandidatePort.isExistsByRoomId(roomId)) {
+            List<Station> stations = getRecommendStationUseCase.getRecommendedStations(roomId);
+            List<Candidate> candidates = stations.stream()
+                .map(station -> new Candidate(roomId, station.getId()))
+                .toList();
+            saveCandidatePort.saveAll(candidates);
+        }
+        return loadCandidatePort.findByRoomId(roomId);
+    }
+}
