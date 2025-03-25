bb4f1fac467c0ced73b599d1365d28c5fe48f4b1
linirini <2001yerin@naver.com>
2025-03-26 00:57:34 +0900

    :sparkle: feat: implement creating Vote Controller and facade service

--- kok-api/src/main/java/com/kok/kokapi/vote/adapter/in/web/VoteController.java
+++ kok-api/src/main/java/com/kok/kokapi/vote/adapter/in/web/VoteController.java
@@ -2,6 +2,7 @@ package com.kok.kokapi.vote.adapter.in.web;
 
 import com.kok.kokapi.common.response.ApiResponseDto;
 import com.kok.kokapi.config.annotion.V1Controller;
+import com.kok.kokapi.vote.adapter.in.dto.request.VoteRequest;
 import com.kok.kokapi.vote.adapter.in.dto.response.CandidateResponse;
 import com.kok.kokapi.vote.application.service.VoteFacadeService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @V1Controller
 @RequiredArgsConstructor
@@ -23,5 +26,16 @@ public class VoteController {
         @PathVariable String roomId, @PathVariable String memberId) {
         List<CandidateResponse> responses = voteFacadeService.getCandidates(roomId, memberId);
         return ResponseEntity.ok(ApiResponseDto.success(responses));
+    }
+
+    @Operation(summary = "투표하기", description = "방 ID와 사용자 ID에 대한 찬성 투표 목록을 받아서 투표 정보를 저장합니다.")
+    @PostMapping("/votes/{roomId}/{memberId}")
+    public ResponseEntity<ApiResponseDto<Void>> createVote(
+        @PathVariable String roomId,
+        @PathVariable String memberId,
+        @RequestBody VoteRequest voteRequest
+    ) {
+        voteFacadeService.createVote(roomId, memberId, voteRequest);
+        return ResponseEntity.ok(ApiResponseDto.success(null));
     }
 }
--- kok-api/src/main/java/com/kok/kokapi/vote/application/service/VoteFacadeService.java
+++ kok-api/src/main/java/com/kok/kokapi/vote/application/service/VoteFacadeService.java
@@ -4,13 +4,17 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kok.kokapi.public_transportation.adapter.in.dto.TmapPublicTransportationParsedResponse;
 import com.kok.kokapi.public_transportation.application.service.TmapPublicTransportationService;
+import com.kok.kokapi.vote.adapter.in.dto.request.VoteRequest;
 import com.kok.kokapi.vote.adapter.in.dto.response.CandidateResponse;
 import com.kok.kokcore.station.application.usecase.GetStationUseCase;
 import com.kok.kokcore.station.application.usecase.RetrieveRouteUseCase;
 import com.kok.kokcore.station.domain.entity.Route;
 import com.kok.kokcore.station.domain.entity.Station;
 import com.kok.kokcore.vote.domain.Candidate;
+import com.kok.kokcore.vote.domain.Vote;
+import com.kok.kokcore.vote.domain.vo.VoteStatus;
 import com.kok.kokcore.vote.usecase.GetCandidateUseCase;
+import com.kok.kokcore.vote.usecase.SaveVoteUseCase;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +27,7 @@ public class VoteFacadeService {
     private final GetCandidateUseCase getCandidateUseCase;
     private final GetStationUseCase getStationUseCase;
     private final RetrieveRouteUseCase retrieveRouteUseCase;
+    private final SaveVoteUseCase saveVoteUseCase;
     private final TmapPublicTransportationService tmapPublicTransportationService;
     private final ObjectMapper objectMapper;
 
@@ -40,7 +45,8 @@ public class VoteFacadeService {
     }
 
     private TmapPublicTransportationParsedResponse getTransportationParsedResponse(
-        String roomId, String memberId, Station station) {
+        String roomId, String memberId, Station station
+    ) {
         String content = tmapPublicTransportationService.retrievePublicTransportation(
             station.getId(),
             roomId,
@@ -54,4 +60,26 @@ public class VoteFacadeService {
         }
     }
 
+    public void createVote(String roomId, String memberId, VoteRequest voteRequest) {
+        List<Vote> votes = getVotes(roomId, memberId, voteRequest);
+        saveVoteUseCase.saveVotes(votes);
+    }
+
+    private List<Vote> getVotes(String roomId, String memberId, VoteRequest voteRequest) {
+        List<Vote> votes = new ArrayList<>();
+        List<Long> agreedStationIds = voteRequest.agreedStationIds();
+        List<Candidate> candidates = getCandidateUseCase.getCandidate(roomId);
+        for (Candidate candidate : candidates) {
+            if (isAgree(agreedStationIds, candidate)) {
+                votes.add(new Vote(candidate, memberId, VoteStatus.AGREE));
+                continue;
+            }
+            votes.add(new Vote(candidate, memberId, VoteStatus.DISAGREE));
+        }
+        return votes;
+    }
+
+    private static boolean isAgree(List<Long> agreedStationIds, Candidate candidate) {
+        return agreedStationIds.contains(candidate.getStationId());
+    }
 }
