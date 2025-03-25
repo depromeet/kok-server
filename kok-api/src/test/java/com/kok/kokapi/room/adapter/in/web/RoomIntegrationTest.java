3bd877c98873a915d6f8890236faf98d39ecb653
linirini <101927543+linirini@users.noreply.github.com>
2025-03-25 17:43:29 +0900

    ✨ [Feature/RoomParticipant] add boolean field if room is full for getting profile list (#76)
    * :recycle: refactor: remove isFulled field from room detail response
    
    * :sparkles: feat: add isFull field for getting profile list for room

--- kok-api/src/test/java/com/kok/kokapi/room/adapter/in/web/RoomIntegrationTest.java
+++ kok-api/src/test/java/com/kok/kokapi/room/adapter/in/web/RoomIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.kok.kokapi.room.adapter.in.web;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 
 import com.kok.kokapi.centroid.adapter.in.dto.request.LocationRequest;
@@ -32,14 +33,15 @@ class RoomIntegrationTest extends IntegrationTest {
 
             inputLocation("방장이 출발지 정보를 입력한다.", createRoomResponse),
 
-            getRoomDetail("약속방 정보를 조회해보면 미참여자는 1명이고, isFulled는 false이다.", createRoomResponse, 1,
-                false),
+            getRoomDetail("약속방 정보를 조회해보면 미참여자는 1명이다", createRoomResponse, 1),
 
             DynamicTest.dynamicTest("팔로워가 약속방에 참여한다.",
                 () -> joinRoomResponse.set(joinRoom(createRoomResponse.get().id(),
                     new JoinRoomParticipantRequest("profile", "follower")))),
 
-            getRoomDetail("약속방 정보를 조회해보면 미참여자는 0명이고, isFulled는 true이다.", createRoomResponse, 0,
+            getRoomDetail("약속방 정보를 조회해보면 미참여자는 0명이다.", createRoomResponse, 0),
+
+            getRoomMembers("약속방 프로필 목록을 조회하면 isFulㅣ은 true이고, 2명의 프로필이 있다", createRoomResponse, 2,
                 true),
 
             checkVoteMode("아직 출발지 입력을 완료하지 않았기에 voteMode는 false이다.", createRoomResponse, false),
@@ -89,8 +91,7 @@ class RoomIntegrationTest extends IntegrationTest {
     }
 
     private static DynamicTest getRoomDetail(String message,
-        AtomicReference<RoomCreateResponse> createRoomResponse, int nonParticipantCount,
-        boolean expectedIsFulled) {
+        AtomicReference<RoomCreateResponse> createRoomResponse, int nonParticipantCount) {
         return DynamicTest.dynamicTest(message,
             () -> {
                 String roomId = createRoomResponse.get().id();
@@ -99,8 +100,23 @@ class RoomIntegrationTest extends IntegrationTest {
                     .when().get("/v1/api/rooms/" + roomId)
                     .then().log().all()
                     .assertThat().statusCode(200)
-                    .body("data.nonParticipantCount", is(nonParticipantCount))
-                    .body("data.isFulled", is(expectedIsFulled));
+                    .body("data.nonParticipantCount", is(nonParticipantCount));
+            });
+    }
+
+    private static DynamicTest getRoomMembers(String message,
+        AtomicReference<RoomCreateResponse> createRoomResponse, int profileCount,
+        boolean expectedIsFull) {
+        return DynamicTest.dynamicTest(message,
+            () -> {
+                String roomId = createRoomResponse.get().id();
+                RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .when().get("/v1/api/rooms/" + roomId + "/participants")
+                    .then().log().all()
+                    .assertThat().statusCode(200)
+                    .body("data.members", hasSize(profileCount))
+                    .body("data.isFull", is(expectedIsFull));
             });
     }
 
