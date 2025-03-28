6beecfe7e343a444655ee86ef7b48d2698049e0e
linirini <2001yerin@naver.com>
2025-03-29 05:18:50 +0900

    :recycle: refactor: move finding members by room id logic from service to port

--- kok-api/src/main/java/com/kok/kokapi/room/adapter/out/persistence/RoomParticipantQueryRedisAdapter.java
+++ kok-api/src/main/java/com/kok/kokapi/room/adapter/out/persistence/RoomParticipantQueryRedisAdapter.java
@@ -1,6 +1,11 @@
 package com.kok.kokapi.room.adapter.out.persistence;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kok.kokcore.room.domain.Member;
 import com.kok.kokcore.room.port.out.LoadRoomParticipantPort;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
@@ -12,6 +17,7 @@ public class RoomParticipantQueryRedisAdapter implements LoadRoomParticipantPort
     public static final String PARTICIPANT_KEY_PREFIX = "room:participants:";
 
     private final RedisTemplate<String, String> redisTemplate;
+    private final ObjectMapper objectMapper;
 
     @Override
     public Long countParticipantsById(String roomId) {
@@ -20,6 +26,23 @@ public class RoomParticipantQueryRedisAdapter implements LoadRoomParticipantPort
             return 0L;
         }
         return redisTemplate.opsForList().size(key);
+    }
+
+    @Override
+    public List<Member> findMembersByRoomId(String roomId) {
+        String key = PARTICIPANT_KEY_PREFIX + roomId;
+        List<String> memberJson = redisTemplate.opsForList().range(key, 0, -1);
+        List<Member> members = new ArrayList<>();
+        if (memberJson != null) {
+            for (String data : memberJson) {
+                try {
+                    Member member = objectMapper.readValue(data, Member.class);
+                    members.add(member);
+                } catch (JsonProcessingException ignored) {
+                }
+            }
+        }
+        return members;
     }
 
     private String buildKey(String roomId) {
--- kok-core/src/main/java/com/kok/kokcore/room/port/out/LoadRoomParticipantPort.java
+++ kok-core/src/main/java/com/kok/kokcore/room/port/out/LoadRoomParticipantPort.java
@@ -1,6 +1,11 @@
 package com.kok.kokcore.room.port.out;
 
+import com.kok.kokcore.room.domain.Member;
+import java.util.List;
+
 public interface LoadRoomParticipantPort {
 
     Long countParticipantsById(String roomId);
+
+    List<Member> findMembersByRoomId(String roomId);
 }
