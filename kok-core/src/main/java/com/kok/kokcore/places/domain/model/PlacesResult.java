7112d72dfaef6eb0cd1b0864199999748ff1189d
yunyoung1819 <yunyoung1819@hanmail.net>
2025-03-28 20:37:25 +0900

    :recycle: refactor: refactoring code review

--- kok-core/src/main/java/com/kok/kokcore/places/domain/model/PlacesResult.java
+++ kok-core/src/main/java/com/kok/kokcore/places/domain/model/PlacesResult.java
@@ -2,4 +2,6 @@ package com.kok.kokcore.places.domain.model;
 
 import java.util.List;
 
-public record PlacesResult(List<Place> places) { }
+public record PlacesResult(
+    List<Place> places
+) { }
