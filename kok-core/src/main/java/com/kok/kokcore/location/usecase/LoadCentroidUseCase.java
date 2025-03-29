2596f963f4859c2c1fdad14442181a762f997953
yunyoung1819 <yunyoung1819@hanmail.net>
2025-03-29 19:06:31 +0900

    :bug: fix: resolve conflict

--- kok-core/src/main/java/com/kok/kokcore/location/usecase/LoadCentroidUseCase.java
+++ kok-core/src/main/java/com/kok/kokcore/location/usecase/LoadCentroidUseCase.java
@@ -6,12 +6,7 @@ import org.springframework.data.util.Pair;
 
 public interface LoadCentroidUseCase {
 
-<<<<<<<< HEAD:kok-core/src/main/java/com/kok/kokcore/location/usecase/LoadCentroidUseCase.java
     Point readCentroid(String roomId);
-========
-public interface ReadCentroidUseCase {
-    Point readCentroid(String uuid);
->>>>>>>> 5c76cf9 (:sparkles: feat: add station recommend):kok-core/src/main/java/com/kok/kokcore/location/usecase/ReadCentroidUseCase.java
 
     Pair<BigDecimal, BigDecimal> readCentroidCoordinates(String roomId);
 }
